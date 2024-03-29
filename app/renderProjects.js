9a57541aa2ff07f9089329edf838d4efcaea53ee
Josue <xJaramis@gmail.com>
2024-03-29 04:45:28 -0700

    Small update

--- app/renderProjects.js
+++ app/renderProjects.js
@@ -1,2 +1,12 @@
+getProjects()
 
-// fetch(data, URL("./resources/data/projects.json"))
+async function getProjects(){
+    const resp =  await fetch("./resources/data/projects.json")
+    const projects = await resp.json();
+    
+    renderProjects(projects);
+}
+
+const renderProjects = (projects) => {
+    console.log(projects);
+}
