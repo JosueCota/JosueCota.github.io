a89b77a588c817a3efadec172c5233e745cf8da0
Josue <xJaramis@gmail.com>
2024-03-31 01:27:52 -0700

    CSS Rehaul (pending), added better btn to Projects

--- app/interactivity.js
+++ app/interactivity.js
@@ -2,15 +2,15 @@
 function showProjectSkills(event){
     const skillDiv = document.getElementById(`project-skills${event.target.id.slice(-1)}`);
 
-    if (event.target.innerHTML=== "Show Skills Used +"){
-        event.target.innerHTML = "Hide Skills -";
+    if (event.target.innerHTML=== "+"){
+        event.target.innerHTML = "-";
         event.target.style.fontSize = ".75rem"
         event.target.style.marginBottom = "1rem"
 
         skillDiv.style.display = "flex";
 
     } else {
-        event.target.innerHTML = "Show Skills Used +";
+        event.target.innerHTML = "+";
         event.target.style.fontSize = "1rem";
         event.target.style.marginBottom = ".25rem"
 
--- app/renderProjects.js
+++ app/renderProjects.js
@@ -8,7 +8,7 @@ fetch("./resources/data/projects.json")
     .then(projects => renderProjects(projects))
     .catch(error => console.error('Error fetching questions:', error));
 
-
+// Render all projects based on amount in projects.json
 const renderProjects = (projects) => {
     for (const project of projects) {
         console.log(project);
@@ -16,6 +16,7 @@ const renderProjects = (projects) => {
     }
 }
 
+//Create project div
 const renderProject = (project) => {
     const num = project["project-num"];
 
@@ -25,47 +26,51 @@ const renderProject = (project) => {
     const topDiv = document.createElement("div");
     topDiv.classList.add("project-norm");
     
-    topDiv.innerHTML = `<img
-    class="project-img"
-    src="${project["project-img"]}"
-    />
-    <div class="project-right">
-      <a href="${project["project-link"]}" class="project-link" target="_blank"><h3 class="project-header project-name">${project["project-name"]}</h3></a>
-      <p class="project-description">
-        ${project["project-desc"]}
-      </p>
-    </div>
-    <button
-    class="project-show-skills"
-    id="skills-btn${num}"
-    onclick="showProjectSkills(event)"
-    >Show Skills Used +</button>`
+    if (project["project-img"]) {
+        topDiv.innerHTML = `<img
+        class="project-img"
+        src="${project["project-img"]}"
+        />
+        <div class="project-right">
+          <a href="${project["project-link"]}" class="project-link" target="_blank"><h3 class="project-header project-name">${project["project-name"]}</h3></a>
+          <p class="project-description">
+            ${project["project-desc"]}
+          </p>
+        </div>`
+    } else {
+        topDiv.innerHTML = `<div class="project-full">
+          <a href="${project["project-link"]}" class="project-link" target="_blank"><h3 class="project-header project-name">${project["project-name"]}</h3></a>
+          <p class="project-description">
+            ${project["project-desc"]}
+          </p>
+        </div>`
+    }
 
     projectCont.appendChild(topDiv);
+    
+    const skillsButton = document.createElement("button");
+    skillsButton.className = "project-btn"
+    skillsButton.id=`skills-btn${num}`;
+    skillsButton.innerHTML = "+";
+    skillsButton.onclick = showProjectSkills;
+    projectCont.appendChild(skillsButton);
 
     const skillDiv = document.createElement("div")
-    skillDiv.classList.add(`project-skills`);
+    skillDiv.classList.add(`project-skills-cont`);
     skillDiv.id = `project-skills${num}`
     skillDiv.style.display = "none";
 
-    addSkillDiv(skillDiv, project["project-skills"].languages, "Languages:")
-    addSkillDiv(skillDiv, project["project-skills"].tech, "Technologies")
-    addSkillDiv(skillDiv, project["project-skills"].general, "General:")
+    addSkillDiv(skillDiv, project["project-skills"])
     //placeholder
-
 
     projectCont.appendChild(skillDiv);
     document.getElementById("projects-section").appendChild(projectCont);
 }
 
-const addSkillDiv = (skillDiv, projectList, header) => {
+//Add Skills to list
+const addSkillDiv = (skillDiv, projectList) => {
     const projectLang = document.createElement("div");
-    projectLang.classList.add("project-skill");
-
-    const projHeader = document.createElement("h3");
-    projHeader.classList.add("project-header");
-    projHeader.innerHTML = header;
-    projectLang.appendChild(projHeader);
+    projectLang.classList.add("project-skills");
 
     const skillList = document.createElement("ul");
     skillList.className = "project-list";
@@ -73,16 +78,16 @@ const addSkillDiv = (skillDiv, projectList, header) => {
     for (let i = 0; i < projectList.length; i++) {
         const listItem = document.createElement("li");
         listItem.innerHTML = projectList[i];
-        listItem.style.backgroundColor = randomColor(i);
+        listItem.style.backgroundColor = colorize(i);
         skillList.appendChild(listItem);
     }
 
     projectLang.appendChild(skillList);
-
     skillDiv.appendChild(projectLang);
 }
 
-const randomColor = (i) => {
+//Add color to div (not random, based on order)
+const colorize = (i) => {
     const check = i%5;
     console.log(check)
     switch (check) {
@@ -97,5 +102,4 @@ const randomColor = (i) => {
         default:
             return "#a4e239"
     }
-
 }
