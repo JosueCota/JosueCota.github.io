fe9a476127e2977dbfd01e24b7eb97cff15a9dc9
Josue <xJaramis@gmail.com>
2024-04-14 23:28:41 -0700

    About me finished .. i think

--- app/interactivity.js
+++ app/interactivity.js
@@ -13,6 +13,15 @@ function showProjectSkills(event){
         event.target.style.backgroundImage = "url(resources/imgs/plus-sign.png)";
         skillDiv.style.display = "none";
     }
-
-
 }
+
+const upBtn = document.getElementById("goUp");
+
+window.addEventListener("scroll", function(){ 
+    console.log(this.window.scrollY);
+    if (window.scrollY >= 200) {
+        upBtn.style.display = "block";
+    } else {
+        upBtn.style.display = "none";
+    }
+});
